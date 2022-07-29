2c2a36ca14bd7bcbdf8dded101e32d9ae93620f8
Anzh <you@example.com>
2022-07-29 04:10:17 +0300

    modified:   src/index.html 	modified:   src/js/side-form.js 	modified:   src/scss/brands.css 	new file:   src/scss/fonts.css 	modified:   src/scss/header.css 	modified:   src/scss/nav.css 	modified:   src/scss/style.scss 	modified:   src/scss/styles-menu.css 	modified:   src/scss/styles.css 	modified:   src/scss/swiper.css

--- src/js/side-form.js
+++ src/js/side-form.js
@@ -8,21 +8,21 @@ const container = document.querySelector('.container')
 
 sideButtonCall.addEventListener('click', function () {
     sideFormCall.classList.add('hidden');
-    container.classList.remove('side-shadow');
+    container.classList.remove('side-shadow-form');
 })
 
 sideButtonFeedback.addEventListener('click', function () {
     sideFormFeedback.classList.add('hidden');
-    container.classList.remove('side-shadow');
+    container.classList.remove('side-shadow-form');
 })
 
 buttonCall.addEventListener('click', function () {
     sideFormCall.classList.remove('hidden');
-    container.classList.add('side-shadow');
+    container.classList.add('side-shadow-form');
     
 })
 
 buttonFeedback.addEventListener('click', function () {
     sideFormFeedback.classList.remove('hidden');
-    container.classList.add('side-shadow');
+    container.classList.add('side-shadow-form');
 })
